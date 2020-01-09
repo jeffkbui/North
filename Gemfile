850fe888aeec5142e39ba1c5e795c07051983a67
Jeffrey Bui <jeffkbui@gmail.com>
2020-01-09 14:46:51 -0800

    set up backend for contact form;

--- Gemfile
+++ Gemfile
@@ -9,7 +9,7 @@ gem 'pry-rails'
 gem 'annotate'
 gem 'jquery-rails'
 gem 'mail_form'
-gem 'dotenv-rails', groups: [:development, :test]
+gem 'dotenv'
 
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
 gem 'rails', '~> 5.2.3'
--- app/controllers/contact_controller.rb
+++ app/controllers/contact_controller.rb
@@ -1,4 +1,17 @@
 class ContactController < ApplicationController
+
   def index
   end
+
+  def create
+      @contact = Contact.new(params[:contact][:data])
+      name = params[:contact][:data][:name]
+      @contact.request = request
+      if @contact.deliver
+        render json: {message: "Thank you for your email, we will contact you shortly!"}                         {name.capitalize}. I will get back to you shortly!”}
+      else
+        render json: {message: 'Error occured. Please try again!'}
+      end
+   end
+
 end
