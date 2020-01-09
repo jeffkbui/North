4036e1b47d9e22de5b262db735272f4f7b69ca0e
Jeffrey Bui <jeffkbui@gmail.com>
2020-01-09 15:21:15 -0800

    mistakes

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,14 +1,14 @@
 class Contact < MailForm::Base
-    attribute :name, :validate => true
-    attribute :email, :validate => /\A([\w\.%\+\-]+)@([\w\-]+\.)+([\w]{2,})\z/i
-    attribute :message
-    attribute :nickname, :captcha => true
+    # attribute :name, :validate => true
+    # attribute :email, :validate => /\A([\w\.%\+\-]+)@([\w\-]+\.)+([\w]{2,})\z/i
+    # attribute :message
+    # attribute :nickname, :captcha => true
 
-    def headers
-        {
-        :subject => 'NORTH Customer Inquiry',
-        :to => 'info@north-losgatos.com',
-        :from => %(“#{name}” <#{email}>)
-        }
-    end
+    # def headers
+    #     {
+    #     :subject => 'NORTH Customer Inquiry',
+    #     :to => 'info@north-losgatos.com',
+    #     :from => %(“#{name}” <#{email}>)
+    #     }
+    # end
 end
--- config/environments/development.rb
+++ config/environments/development.rb
@@ -7,18 +7,18 @@ Rails.application.configure do
   config.cache_classes = false
 
   #mailer contact us form
-  config.action_mailer.default_url_options = { :host => ‘localhost:3001’ }
-  config.action_mailer.raise_delivery_errors = true
+  # config.action_mailer.default_url_options = { :host => ‘localhost:3000’ }
+  # config.action_mailer.raise_delivery_errors = true
 
-  config.action_mailer.smtp_settings = {
-    address: 'smtp.gmail.com',
-    port: 587,
-    domain: ENV['GMAIL_DOMAIN'],
-    authentication: 'plain',
-    enable_starttls_auto: true,
-    user_name: ENV['GMAIL_USERNAME'],
-    password: ENV['GMAIL_PASSWORD']
-  }
+  # config.action_mailer.smtp_settings = {
+  #   address: 'smtp.gmail.com',
+  #   port: 587,
+  #   domain: ENV['GMAIL_DOMAIN'],
+  #   authentication: 'plain',
+  #   enable_starttls_auto: true,
+  #   user_name: ENV['GMAIL_USERNAME'],
+  #   password: ENV['GMAIL_PASSWORD']
+  # }
 
   # Do not eager load code on boot.
   config.eager_load = false
